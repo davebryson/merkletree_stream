9ea0a156b354b572b01fda2e945cca1d7e1c1865
Dave Bryson <daveb@miceda.org>
2021-06-07 10:35:42 -0500

    tests

--- test/tree_tests.erl
+++ test/tree_tests.erl
@@ -3,19 +3,13 @@
 -include("./src/merkletree_stream.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-create_tree(Data) ->
-    buildit(Data, merkletree_stream:new()).
-buildit([], State) ->
-    State;
-buildit([H | T], State) ->
-    buildit(T, merkletree_stream:write(H, State)).
-
 %%%
 %%% Tests
 %%%
+
 build_with_single_root_test() ->
     Data = [<<"a">>, <<"b">>, <<"c">>],
-    Tree = create_tree(Data),
+    Tree = merkletree_stream:create_tree(Data),
     ?assertEqual(2, length(Tree#state.roots)),
     ?assertEqual(4, length(Tree#state.nodes)),
     ?assertEqual(3, Tree#state.blocks),
@@ -23,7 +17,7 @@ build_with_single_root_test() ->
 
 build_more_than_one_root_test() ->
     Data = [<<"a">>, <<"b">>, <<"c">>, <<"d">>],
-    Tree = create_tree(Data),
+    Tree = merkletree_stream:create_tree(Data),
     ?assertEqual(1, length(Tree#state.roots)),
     ?assertEqual(7, length(Tree#state.nodes)),
     ?assertEqual(4, Tree#state.blocks),
@@ -52,7 +46,7 @@ deterministic_even_test() ->
             <<>>, 2}
     ],
     Data = [<<"a">>, <<"b">>],
-    Tree = create_tree(Data),
+    Tree = merkletree_stream:create_tree(Data),
     ?assertEqual(ExpectedNodes, Tree#state.nodes),
     ?assertEqual(ExpectedRoot, Tree#state.roots),
     ok.
@@ -92,7 +86,7 @@ deterministic_odd_test() ->
     ],
 
     Data = [<<"a">>, <<"b">>, <<"c">>],
-    Tree = create_tree(Data),
+    Tree = merkletree_stream:create_tree(Data),
     ?assertEqual(ExpectedNodes, Tree#state.nodes),
     ?assertEqual(ExpectedRoots, Tree#state.roots),
     ok.
